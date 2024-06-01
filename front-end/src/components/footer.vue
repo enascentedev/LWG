be2df3e7b18f1c5975548f41a912c07bdb52cb8e
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-05-31 22:03:14 -0300

    alterando links footer

--- front-end/src/components/footer.vue
+++ front-end/src/components/footer.vue
@@ -2,10 +2,10 @@
 	<footer>
 		<div id="container">
 			<p>&copy; 2023 , Todos os direitos reservados.</p>
-			<a href="#"><i class="fab fa-facebook"></i></a>
-			<a href="#"><i class="fab fa-twitter"></i></a>
-			<a href="#"><i class="fab fa-github"></i></a>
-			<a href="#"><i class="fab fa-youtube"></i></a>
+			<a href="https://www.linkedin.com/in/emanuel-nascente-3b36b122a/"><i class="fa-brands fa-linkedin"></i></a>
+			<a href="https://twitter.com/EmanuelNascente"><i class="fab fa-twitter"></i></a>
+			<a href="https://github.com/enascentedev"><i class="fab fa-github"></i></a>
+			<a href="https://www.wgospel.com/"><i class="fab fa-youtube"></i></a>
 		</div>
 	</footer>
 </template>
